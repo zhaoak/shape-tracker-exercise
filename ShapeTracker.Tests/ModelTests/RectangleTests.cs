c2a180931390626bf1b95e4835ec1731fb5af51f
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-10 15:57:37 -0700

    added rectangle height and width properties, tested

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -7,11 +7,24 @@ namespace ShapeTracker.Tests
   public class RectangleTests
   {
     // tests
+    // test if constructor constructs rectangle object
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
       Rectangle newRectangle = new Rectangle(4, 6);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
+    }
+
+    // test if can set and retrieve rectangle side length vals
+    [TestMethod]
+    public void GetSides_ReturnsSides_Int()
+    {
+      int length1 = 4;
+      int length2 = 2;
+
+      Rectangle newRectangle = new Rectangle(length1, length2);
+      Assert.AreEqual(length1, newRectangle.Width);
+      Assert.AreEqual(length2, newRectangle.Height);
     }
   }
 }
--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -4,7 +4,11 @@ namespace ShapeTracker.Models{
     {
        public Rectangle(int length1, int length2)
        {
-            
+            Width = length1;
+            Height = length2;
        }
+
+       public int Width { get; set; }
+       public int Height { get; set; }
     }
 }
